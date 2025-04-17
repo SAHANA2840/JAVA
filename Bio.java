a4e4b1c8d1aafcce277ad2a80bf0e8272df6138d
SAHANA2840 <mailtosahana28@gmail.com>
2025-04-17 20:50:38 +0530

    task

---
+++ Bio.java
@@ -0,0 +1,29 @@
+class Bio 
+{
+public static void main(String args[])
+{
+System.out.println("  \t\t\tBIODATA");
+System.out.println("  \t\t\t\t\t\t\t_________");
+System.out.println("  \t\t\t\t\t\t\t|\t|"); 
+System.out.println("  \t\t\t\t\t\t\t|\t|");
+System.out.println("  Name            :   _______________________\t\t|\t|");
+System.out.println("  Mobile          :   _______________________\t\t|\t|");
+System.out.println("  Emailid         :   _______________________\t\t|_______|");
+System.out.println("  Father's name   :   _______________________");
+System.out.println("  Gender          :   _______________________");
+System.out.println("  Date of Birth   :   _______________________");
+System.out.println("  Martial status  :   _______________________");
+System.out.println("  Religion        :   _______________________");
+System.out.println("  Languages Known :   _______________________");
+System.out.println("  Qualifiaction   :   _____________________________________");
+System.out.println("  \t\t      _____________________________________");
+System.out.println("  \t\t      _____________________________________");
+System.out.println("  Experience      :   _____________________________________");
+System.out.println("  \t\t      _____________________________________");
+System.out.println("  Address         :   _____________________________________");
+System.out.println("  \t\t      _____________________________________");
+System.out.println("  Place:");
+System.out.println("  Date:\t\t\t\t_____________");
+System.out.println("  \t\t\t\t  Signature");
+}
+}
---
+++ gb.java
@@ -0,0 +1,30 @@
+class gb
+{
+public static void main(String args[])
+{
+System.out.println("  **********************************");
+System.out.println("  \t     RECEIPT");
+System.out.println("  **********************************");
+System.out.println("  Terminal#1\t01-02-2018  11:32");
+System.out.println("  ----------------------------------");
+System.out.println("  1 X T-Shirt\t\t     $25.50");
+System.out.println("  2 X Watches\t\t    $299.00");
+System.out.println("  3 X Pants\t\t     $32.99");
+System.out.println("  4 X Socks\t\t      $6.50");
+System.out.println("  ==================================");
+System.out.println("  TOTAL AMOUNT\t\t    $363.99");
+System.out.println("  CASH\t\t\t    $400.00");
+System.out.println("  CHANGE\t\t     $36.01");
+System.out.println();
+System.out.println("  Bank Card\t**** **** **** 7211");
+System.out.println("  Approval#\t\t     192107");
+System.out.println("  ----------------------------------");
+System.out.println();
+System.out.println("  ************THANK YOU!************");
+System.out.println("");
+System.out.println(".......................................");
+System.out.println("   | || | || || | | || | || || | ||");
+System.out.println("   | || | || || | | || | || || | ||");
+System.out.println("   | || | || || | | || | || || | ||");
+}
+}
